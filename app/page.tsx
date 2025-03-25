adc46fdf413fb062d6f2f4e306423d7ea02167a3
abhasMishra2025 <abhas.mishra@vaidik-edu.com>
2025-03-25 21:20:42 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import MainPage from '@/components/main-page';
+
+export default function Page() {
+    return (
+        <div>
+            <MainPage />
+        </div>
+    );
+}
